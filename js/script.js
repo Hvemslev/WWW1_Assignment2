025f05e511cc7f214c4c24e9dd1ed92d4e1b710a
Hvemslev <111503814+Hvemslev@users.noreply.github.com>
2026-04-15 21:06:47 +0200

    click "animation"

--- js/script.js
+++ js/script.js
@@ -1,6 +1,27 @@
 var points = 0;
+var diameter = 5
 
 document.querySelector(".circle").addEventListener("click", function(){
     points++
     document.getElementById("Score").innerText=points+"!"
+    diameter = 5.5
 })
+
+
+
+
+function update() {
+  requestAnimationFrame(() => {
+    if(diameter>5){
+        diameter-=0.1
+    }
+    document.querySelector(".circle").style.width=diameter+"rem"
+    document.querySelector(".circle").style.height=diameter+"rem"
+
+    update();
+  });
+}
+update();
+
+
+
